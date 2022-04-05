c4f3cd5a834fe64584a7f0cd50220128ac085113
daryllaraya <daryllaraya@gmail.com>
2022-04-05 12:51:45 +0800

    modified the sass partials/architecture, replaced the @import with @use and @forward

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const autoprefixer = require('gulp-autoprefixer');
 const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
-  return src('lp/**/*.scss')
+  return src('sass/**/*.scss')
     .pipe(sass())
     .pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
     .pipe(purgecss({ content: ['*.html'] }))
@@ -12,7 +12,7 @@ function buildStyles() {
 }
 
 function watchTask() {
-  watch(['lp/**/*.scss', '*.html'], buildStyles)
+  watch(['sass/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
